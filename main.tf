611accc34a83e0f374a19555c832491c9ec0f805
Wolf Loescher <wolf.loescher@gmail.com>
2026-09-23 10:03:15 -0700

    Refactor variables to variables.tf

--- main.tf
+++ main.tf
@@ -12,23 +12,6 @@ provider "azurerm" {
   resource_provider_registrations = "core"
 }
 
-variable "prefix" {}
-
-variable "region" {
-  type        = string
-  default     = "UK South"
-  description = "The Azure region to deploy resources"
-  validation {
-    condition     = contains(["UK South", "UK West", "North Europe", "West Europe", "East US", "West US"], var.region)
-    error_message = "Invalid region"
-  }
-}
-
-variable "tags" {
-  type        = map(any)
-  description = "A map of tags"
-}
-
 resource "azurerm_resource_group" "contoso_rg" {
   name     = "${var.prefix}_rg"
   location = var.region
