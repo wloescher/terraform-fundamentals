a27009562bebf3f65ce07647ca4c7b95f4fa7fc9
Wolf Loescher <wolf.loescher@gmail.com>
2026-09-23 09:59:50 -0700

    Environment Variables and custom .tfvars

--- main.tf
+++ main.tf
@@ -12,22 +12,31 @@ provider "azurerm" {
   resource_provider_registrations = "core"
 }
 
-locals {
-  prefix = "contoso"
-  region = "North Europe"
-  tags = {
-    cost_center = "contoso research"
+variable "prefix" {}
+
+variable "region" {
+  type        = string
+  default     = "UK South"
+  description = "The Azure region to deploy resources"
+  validation {
+    condition     = contains(["UK South", "UK West", "North Europe", "West Europe", "East US", "West US"], var.region)
+    error_message = "Invalid region"
   }
 }
 
+variable "tags" {
+  type        = map(any)
+  description = "A map of tags"
+}
+
 resource "azurerm_resource_group" "contoso_rg" {
-  name     = "${local.prefix}_rg"
-  location = local.region
-  tags     = local.tags
+  name     = "${var.prefix}_rg"
+  location = var.region
+  tags     = var.tags
 }
 
 resource "azurerm_resource_group" "contoso_dev_rg" {
-  name     = "${local.prefix}_dev_rg"
-  location = local.region
-  tags     = local.tags
+  name     = "${var.prefix}_dev_rg"
+  location = var.region
+  tags     = var.tags
 }
